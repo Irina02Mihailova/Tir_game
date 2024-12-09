7ef7380a9b4e3b083b496306b775bf7d01762d8c
Irina02Mihailova <imihailova193@gmail.com>
2024-12-09 20:49:56 +0500

    add cycle

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ target_width = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-
+#цвет фона
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+# Переменная для подсчета очков
+score = 0
+font = pygame.font.Font(None, 36)
 
 running = True
 while running:
@@ -30,10 +33,14 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                score += 1
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
     screen.blit(target_img, (target_x, target_y))
+    # Отображение очков
+    score_text = font.render(f'Очки: {score}', True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))  # Отображение в верхнем левом углу
     pygame.display.update()
 
 pygame.quit()
