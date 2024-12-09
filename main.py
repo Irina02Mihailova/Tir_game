adaefb8b95cc3fc6810b780f155d7bab8c64eda5
Irina02Mihailova <imihailova193@gmail.com>
2024-12-09 20:46:17 +0500

    add cycle code

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from turtledemo.nim import SCREENWIDTH, SCREENHEIGHT
-
 import pygame
 import random
-from pygame.examples.scrap_clipboard import screen
 
 pygame.init()
 
@@ -16,7 +13,7 @@ pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/1.jpg")
 pygame.display.set_icon(icon)
 #цель
-target_img = pygame.image.load("img/2.png")
+target_img = pygame.image.load("img/15593439.png")
 target_width = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
@@ -24,8 +21,19 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-runnig = True
-while runnig:
-    pass
+running = True
+while running:
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
