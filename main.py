2a9edbf2d46b54ff1acca233c3f4e659e9811d7c
Irina02Mihailova <imihailova193@gmail.com>
2024-12-09 20:27:21 +0500

    add const-code

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+from turtledemo.nim import SCREENWIDTH, SCREENHEIGHT
+
 import pygame
+import random
+from pygame.examples.scrap_clipboard import screen
 
 pygame.init()
+
+#размер экрана
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+#заголовок
+pygame.display.set_caption("Игра Тир")
+#иконка
+icon = pygame.image.load("img/1.jpg")
+pygame.display.set_icon(icon)
+#цель
+target_img = pygame.image.load("img/2.png")
+target_width = 80
+target_height = 80
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 runnig = True
 while runnig:
